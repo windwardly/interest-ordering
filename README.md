20e6f8e5a56c76b3ba6e786a197e33ad18e221e1
Windwardly <windwardly@users.noreply.github.com>
2022-06-26 08:17:51 -0400

    add model simulation

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# interest-ordering
-explore impact of different orderings of array of interest rates
+# Explore how the order of interest rates impact accumulated savings
+
+While not accurately reflecting a typical family's income and spending,
+it does show that order matters; there is a consequence
+for delaying income advance in the face of rising costs.
+
+## Model
+
+Initial conditions: a base monthly income and a base monthly level of expenditures.
+
+Two arrays: The annual rate of pay increase and spending increase cumulatively applied
+the inital base rates.
+
+A savings account that has a fixed (ie, unrealistic) annual interest rate, compounded monthly.
+
+## The outcome
+
+The amount in the savings account at the end of the N year simulation as a function of differently sorting the arrays.
+
+Worst outcome: delay raises by sorting the income rates from low to high; accelerate costs by sorting expenditure rates high to low.
+
+Best: the reverse of worst.
+
+Actual: use the arrays as listed.
+
+## Additionally: the Baumol Effect
+
+The Baumol Effect is experienced by certain organizations and populations that experience inflation and a higher rate than the average organization or
+population.
+
+The impact is the magic of compound interest in reverse.
+
+In the model, this can be explored by having the two arrays be the same or different.
---
+++ interest-ordering.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+# Play out a scenario, returning what is in savings account after 10 years
+# month_interest is for the savings account
+# income is bumped every year according to income_rate (inrate)
+# spending is bumped every year according to spending_rate (outrate)
+# Note: the final income and spending levels will be the same for
+# any sorting of inrates and outrates, because it is just multiplying
+# all the rates together times initial income or spending.
+def play_out(income, spend, interest, inrates, outrates):
+    monthly_savings_interest = 1.0 + (interest / (12.0 * 100.0));
+    savings = 0.0;
+    for year in range(len(inrates)):
+        # print("inrates", (inrates[year] / 100.0))
+        income *= 1.0 + (inrates[year] / 100.0)
+        spend *= 1.0 + (outrates[year] / 100.0)
+        for month in range(12):
+            savings += income - spend
+            savings *= monthly_savings_interest
+            # print("Year", year + 1, "Month", month + 1,
+            #    "Income", income, "Spend", spend, "Savings", savings)
+    # uncomment for sanity check that this is the same in all scenarios
+    # print("Income", '${:,.2f}'.format(income),
+    #    "Spending", '${:,.2f}'.format(spend))
+    return savings
+
+# encapsulate python formatting
+def format_list(list):
+    return ', '.join(['{:.2f}'.format(rate) for rate in list])
+
+# The annual percentage rate at which income goes up.
+income_rate = [3.4, 1.8, 1.4, 1.8, 0.1, 0.3, 2.1, 2.6, 1.4, 1.4, 5.5]
+income_rate_lo_to_hi = income_rate.copy()
+income_rate_lo_to_hi.sort()
+income_rate_hi_to_lo = income_rate_lo_to_hi.copy()
+income_rate_hi_to_lo.reverse()
+income_ave = sum(income_rate) / len(income_rate)
+
+# The annual inflation rate experienced.
+spending_rate = [2.8, 2.9, 2.8, 3.1, 3.8, 3.1, 3.2, 3.9, 3.5, 1.6, 2.1]
+spending_rate_lo_to_hi = spending_rate.copy()
+spending_rate_lo_to_hi.sort()
+spending_rate_hi_to_lo = spending_rate_lo_to_hi.copy()
+spending_rate_hi_to_lo.reverse()
+spending_ave = sum(spending_rate) / len(spending_rate)
+
+income = 6000.0;    # monthly income stream
+spend = 3000.0;     # monthly spending
+savings_rates = [0.0, 5.0]
+
+strategies = [
+        ("Worst   ", income_rate_lo_to_hi, spending_rate_hi_to_lo),
+        ("Unsorted", income_rate, spending_rate),
+        ("Best    ", income_rate_hi_to_lo, spending_rate_lo_to_hi),
+    ]
+
+print()
+print("Percent annual increases:")
+print("Income  ", format_list(income_rate))
+print("Average ", format_list([income_ave]))
+print()
+print("Spending", format_list(spending_rate))
+print("Average ", format_list([spending_ave]))
+print()
+print("Ave Diff", format_list([spending_ave - income_ave]))
+print()
+print("Results below show that with no time value of money, order matters.")
+print("And shows that with some actual time value of money, it matters more.")
+print()
+print("This is with starting monthly income of", '${:,.2f}'.format(income))
+print("and with starting monthly spending of", '${:,.2f}'.format(spend))
+print("and with a starting bank account balance of $0.00")
+print()
+print("Bank balance after", len(income_rate), "years")
+print("with different sortings on rate increases")
+for interest in savings_rates:
+    print()
+    print("Interest", '{:.2f}%'.format(interest))
+    for (scenario, inrates, outrates) in strategies:
+        print (scenario, '${:,.2f}'.format(play_out(income, spend, interest, inrates, outrates)))
+print()
+print("Removing Baumol effect (spending inflation = income inflation)")
+for interest in savings_rates:
+    print()
+    print("Interest", '{:.2f}%'.format(interest))
+    for (scenario, inrates, outrates) in strategies:
+        print (scenario, '${:,.2f}'.format(play_out(income, spend, interest, inrates, inrates)))
+print()
